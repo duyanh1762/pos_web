518b900a62185731cc1d25415c1c37e750b51e4e
duyanh1762 <deadpool692.0@gmail.com>
2024-07-23 22:08:38 +0700

    update

--- src/app/manage/sale/sale/sale.component.ts
+++ src/app/manage/sale/sale/sale.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { Bill } from 'src/app/Models/bill';
 import { BillDetail } from 'src/app/Models/bill_detail';
 import { Item } from 'src/app/Models/item';
@@ -27,13 +33,20 @@ interface StaffInfor {
   styleUrls: ['./sale.component.css'],
 })
 export class SaleComponent implements OnInit {
+  @ViewChild('selectStaff', { read: ElementRef, static: false })
+  selectStaff: ElementRef;
+  @ViewChild('selectStatus', { read: ElementRef, static: false })
+  selectStatus: ElementRef;
+  @ViewChild('tableN', { read: ElementRef, static: false }) tableN: ElementRef;
   public shop: Shop;
   public paid: number = 0;
   public unpaid: number = 0;
   public staffs: Array<StaffInfor> = [];
-  public tableNum:number = 0;
+  public tableNum: number = 0;
   // public sum:number=0;
   public bills: Array<BillInfor> = [];
+  public billsLU: Array<BillInfor> = [];
+  public lastFilter: Array<BillInfor> = [];
   constructor(private api: ApiService) {}
 
   ngOnInit(): void {
@@ -51,7 +64,7 @@ export class SaleComponent implements OnInit {
       .toPromise()
       .then((res: any) => {
         res.forEach((s: Staff) => {
-          if(s.shopID === this.shop.id){
+          if (s.shopID === this.shop.id) {
             let si = {
               id: s.id,
               name: s.name,
@@ -101,11 +114,12 @@ export class SaleComponent implements OnInit {
               nameStaff: n,
               total: t,
             };
-           let newDate = this.api.dateTransform(b.date);
-           b.date = newDate;
+            let newDate = this.api.dateTransform(b.date);
+            b.date = newDate;
             this.bills.push(b);
-            this.staffs.forEach((si:StaffInfor)=>{
-              if(b.staffID === si.id && b.status === "pay"){
+            this.billsLU.push(b);
+            this.staffs.forEach((si: StaffInfor) => {
+              if (b.staffID === si.id && b.status === 'pay') {
                 si.total = si.total + b.total;
               }
             });
@@ -119,4 +133,41 @@ export class SaleComponent implements OnInit {
         });
       });
   }
+  filter() {
+    let table = this.tableN.nativeElement.value;
+    let status = this.selectStatus.nativeElement.value;
+    let staff = this.selectStaff.nativeElement.value;
+    if(staff === "all"  && status === "all" && table === ""){
+      this.billsLU = this.bills;
+    }else if(staff === "all" && status === "all" && table != ""){
+      this.billsLU = this.bills.filter((b: BillInfor) => {
+        return b.table === table;
+      });
+    }else if(staff === "all" && status != "all" && table === ""){
+      this.billsLU = this.bills.filter((b: BillInfor) => {
+        return b.status === status;
+      });
+    }else if(staff != "all" && status === "all" && table === ""){
+      this.billsLU = this.bills.filter((b: BillInfor) => {
+        return b.staffID === Number(staff);
+      });
+    }else if(staff != "all" && status != "all" && table === ""){
+      this.billsLU = this.bills.filter((b: BillInfor) => {
+        return b.staffID === Number(staff) && b.status === status;
+      });
+    }else if(staff != "all" && status === "all" && table != ""){
+      this.billsLU = this.bills.filter((b: BillInfor) => {
+        return b.staffID === Number(staff) && b.table === table;
+      });
+    }else if(staff === "all" && status != "all" && table != ""){
+      this.billsLU = this.bills.filter((b: BillInfor) => {
+        return b.status === status && b.table === table;
+      });
+    }else if(staff != "all" && status != "all" && table != ""){
+      this.billsLU = this.bills.filter((b: BillInfor) => {
+        return b.staffID === Number(staff) && b.status === status && b.table === table;
+      });
+    }
+  }
+
 }
