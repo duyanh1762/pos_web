ee11cffbe7852255d046ab60278eae9b4a757d8b
duyanh1762 <deadpool692.0@gmail.com>
2024-07-25 10:45:06 +0700

    update

--- src/app/manage/report/report/report-staff/report-staff/report-staff.component.ts
+++ src/app/manage/report/report/report-staff/report-staff/report-staff.component.ts
@@ -1,33 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Bill } from 'src/app/Models/bill';
+import { Item } from 'src/app/Models/item';
+import { Shop } from 'src/app/Models/shop';
+import { Staff } from 'src/app/Models/staff';
+import { ApiService } from 'src/app/Service/api.service';
+interface SatffInfor{
+  id: number;
+  shopID:number
+  name: string;
+  status: string;
+  total:number;
+}
 @Component({
   selector: 'app-report-staff',
   templateUrl: './report-staff.component.html',
   styleUrls: ['./report-staff.component.css']
 })
 export class ReportStaffComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
   startDate: Date | null = null;
   endDate: Date | null = null;
+  items:Array<Item> = [];
+  staffs:Array<SatffInfor> =[];
+  shop:Shop;
+  bills:Array<Bill>= [];
+  constructor(private api:ApiService) { }
 
-  handleDateChange(event: any) {
-    const date = event.value;
-    const formattedDate = this.formatDate(date);
-    console.log('Selected date:', formattedDate);
+  ngOnInit(): void {
+    this.load();
   }
-
-  formatDate(date: Date): string {
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`;
+  async load(){
+    this.shop = JSON.parse(localStorage.getItem('shop-infor') || '{}');
+    let request = {
+      mode:"get",
+      data:"",
+    };
+    await this.api.getItems(request).toPromise().then((res:any)=>{this.items = res});
+    await this.api.getBill(request).toPromise().then((res:any)=>{this.bills = res});
+    await this.api.getStaff(request).toPromise().then((res:any)=>{
+      res.forEach((s:Staff)=>{
+        if(this.shop.id === s.shopID){
+          let si = {
+            id: s.id,
+            shopID:s.shopID,
+            name: s.name,
+            status: s.status,
+            total:0,
+          }
+          this.staffs.push(si);
+        }
+      });
+    });
+    console.log(this.staffs);
   }
-  getTimeLine(){
-    console.log("Ngay bat dau: "+ this.startDate);
-    console.log("Ngay ket thuc: "+ this.endDate);
+  confirmDate(){
+    let count = 0;
+    console.log(this.bills);
+    this.bills.forEach((b:Bill)=>{
+      let billDate = new Date(b.date);
+      if( this.endDate != null){
+        if( billDate <= this.endDate){
+          count = count + 1;
+          console.log(b);
+          console.log(billDate);
+          console.log(this.endDate);
+        }
+      }
+    });
+    console.log(count);
   }
 }
