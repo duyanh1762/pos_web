5276d679378b18091b78c0c3277583284a683ff9
duyanh1762 <deadpool692.0@gmail.com>
2024-07-25 20:52:36 +0700

    update staff report

--- src/app/manage/report/report/report-staff/report-staff/report-staff.component.ts
+++ src/app/manage/report/report/report-staff/report-staff/report-staff.component.ts
@@ -1,71 +1,116 @@
 import { Component, OnInit } from '@angular/core';
 import { Bill } from 'src/app/Models/bill';
+import { BillDetail } from 'src/app/Models/bill_detail';
 import { Item } from 'src/app/Models/item';
 import { Shop } from 'src/app/Models/shop';
 import { Staff } from 'src/app/Models/staff';
 import { ApiService } from 'src/app/Service/api.service';
-interface SatffInfor{
+interface SatffInfor {
   id: number;
-  shopID:number
+  shopID: number;
   name: string;
   status: string;
-  total:number;
+  total: number;
 }
 @Component({
   selector: 'app-report-staff',
   templateUrl: './report-staff.component.html',
-  styleUrls: ['./report-staff.component.css']
+  styleUrls: ['./report-staff.component.css'],
 })
 export class ReportStaffComponent implements OnInit {
   startDate: Date | null = null;
   endDate: Date | null = null;
-  items:Array<Item> = [];
-  staffs:Array<SatffInfor> =[];
-  shop:Shop;
-  bills:Array<Bill>= [];
-  constructor(private api:ApiService) { }
+  items: Array<Item> = [];
+  staffs: Array<SatffInfor> = [];
+  shop: Shop;
+  bills: Array<Bill> = [];
+  renuvalue:number=0;
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
     this.load();
   }
-  async load(){
+  onStartDateChange(event: any): void {
+    if (event) {
+      this.startDate = new Date(event);
+      this.startDate.setHours(0, 0, 0, 0); // Thiết lập thời gian 00:00:00
+    }
+  }
+
+  onEndDateChange(event: any): void {
+    if (event) {
+      this.endDate = new Date(event);
+      this.endDate.setHours(23, 59, 59, 999); // Thiết lập thời gian 23:59:59
+    }
+  }
+  async load() {
     this.shop = JSON.parse(localStorage.getItem('shop-infor') || '{}');
     let request = {
-      mode:"get",
-      data:"",
+      mode: 'get',
+      data: '',
     };
-    await this.api.getItems(request).toPromise().then((res:any)=>{this.items = res});
-    await this.api.getBill(request).toPromise().then((res:any)=>{this.bills = res});
-    await this.api.getStaff(request).toPromise().then((res:any)=>{
-      res.forEach((s:Staff)=>{
-        if(this.shop.id === s.shopID){
-          let si = {
-            id: s.id,
-            shopID:s.shopID,
-            name: s.name,
-            status: s.status,
-            total:0,
-          }
-          this.staffs.push(si);
-        }
+    await this.api
+      .getItems(request)
+      .toPromise()
+      .then((res: any) => {
+        this.items = res;
       });
-    });
-    console.log(this.staffs);
+    await this.api
+      .getBill(request)
+      .toPromise()
+      .then((res: any) => {
+        res.forEach((b: Bill) => {
+          if (b.shopID === this.shop.id) {
+            this.bills.push(b);
+          }
+        });
+      });
+    await this.api
+      .getStaff(request)
+      .toPromise()
+      .then((res: any) => {
+        res.forEach((s: Staff) => {
+          if (this.shop.id === s.shopID) {
+            let si = {
+              id: s.id,
+              shopID: s.shopID,
+              name: s.name,
+              status: s.status,
+              total: 0,
+            };
+            this.staffs.push(si);
+          }
+        });
+      });
   }
-  confirmDate(){
-    let count = 0;
-    console.log(this.bills);
-    this.bills.forEach((b:Bill)=>{
+  confirmDate() {
+    this.staffs.forEach((si:SatffInfor)=>{
+      si.total = 0;
+    });
+    this.renuvalue = 0;
+    this.bills.forEach((b: Bill) => {
       let billDate = new Date(b.date);
-      if( this.endDate != null){
-        if( billDate <= this.endDate){
-          count = count + 1;
-          console.log(b);
-          console.log(billDate);
-          console.log(this.endDate);
+      if (this.startDate != null && this.endDate != null) {
+        if (billDate >= this.startDate && billDate <= this.endDate) {
+          this.staffs.forEach(async (si: SatffInfor) => {
+            if (si.id === b.staffID) {
+              await this.api
+                .getDetail({ mode: 'get', data: Number(b.id) })
+                .toPromise()
+                .then((res: any) => {
+                  res.forEach((bd: BillDetail) => {
+                    this.items.forEach((i: Item) => {
+                      if (bd.itemID === i.id) {
+                        si.total = si.total + bd.num * i.price;
+                        this.renuvalue = this.renuvalue + bd.num * i.price
+                      }
+                    });
+                  });
+                });
+            }
+          });
         }
       }
     });
-    console.log(count);
   }
 }
